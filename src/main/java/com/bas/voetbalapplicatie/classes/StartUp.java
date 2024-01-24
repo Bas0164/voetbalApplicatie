42fba31a8332be615def27da62e4fb64cbce59dc
Bas de Bruijn <basdebruijn0164@gmail.com>
2024-01-24 13:53:04 +0100

    # JAR file
    ## I checked if I could make a jar file that is runnable

---
+++ src/main/java/com/bas/voetbalapplicatie/classes/StartUp.java
@@ -0,0 +1,9 @@
+package com.bas.voetbalapplicatie.classes;
+
+import com.bas.voetbalapplicatie.Application;
+
+public class StartUp {
+    public static void main(String[] args) {
+        Application.main(args);
+    }
+}
